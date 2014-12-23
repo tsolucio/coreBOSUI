fb378a5f7d44ef15c843b7227b34b22b69d232bd
root <root@opencubed-RV410-RV510-S3510-E3510.(none)>
2014-12-23 15:37:57 +0100

    LESS support parametrization

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -55,8 +55,8 @@ angular.module('coreBOSJSTickets.controllers', [])
 .controller('termscCtrl',function($scope) {
 })
 .controller('loginCtrl',function($scope, $rootScope, $i18next, $filter, Setup, coreBOSWSAPI, coreBOSAPIStatus,$location) {
-	$scope.username='test';
-        $scope.password='juKaW2jesSfBhgHM';
+	$scope.username='admin';
+        $scope.password='sZTFHVYW8Y3d8ix';
         if (!coreBOSAPIStatus.hasInvalidKeys()) {
 		$location.path('/currentorder');
 	}
